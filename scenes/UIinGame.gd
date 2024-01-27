cf9f1922da777a1dad2f6cf05c8d927816d9ff04
volo <89660634+wolo1@users.noreply.github.com>
2024-01-27 18:31:17 +0100

    UI in Game implementation
    UI displaying remaining pigeons

---
+++ scenes/UIinGame.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@export var gameText: RichTextLabel
+var current_bread_count: int = 100
+@export var max_bread_count: int = 100
+
+func _ready():
+	current_bread_count = max_bread_count
+
+
+func _on_bread_thrown():
+	current_bread_count -= 1
+	update_ui()
+	
+
+func update_ui():
+	if current_bread_count <= 0:
+		current_bread_count = 0
+	gameText.text = str(current_bread_count) + "/" + str(max_bread_count)
+
+
+
+	
