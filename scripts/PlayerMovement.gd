21a5be31d1ecd5b1e232801b8d6e48ae3b320c5b
volo <89660634+wolo1@users.noreply.github.com>
2024-01-28 10:38:03 +0100

    Added constrains to player movement
    Now you cant move the player outside the game area

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -2,8 +2,9 @@ extends Node3D
 
 # Movement speed
 var speed = 5.0
-var z_at_last_need_new_block = 1;
+var z_at_last_need_new_block = 1
 var block_size
+var max_distance_x = 2.6  # Maximum distance the player can move to the right or left
 
 @export var block_scene: PackedScene
 
@@ -32,9 +33,15 @@ func _physics_process(delta):
 
 	# Apply the movement
 	position += (velocity * delta)
-	
+
+	# Constrain the player's movement to the left and right
+	if position.x > max_distance_x:
+		position.x = max_distance_x
+	elif position.x < -max_distance_x:
+		position.x = -max_distance_x
+
+	# Check for block spawning
 	var dist_since_last_block_spawn = abs(position.z - z_at_last_need_new_block)
 	if (dist_since_last_block_spawn > block_size):
 		need_new_block.emit()
 		z_at_last_need_new_block -= block_size
-		
