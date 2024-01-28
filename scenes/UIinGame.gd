e29419e72a462e4e76d1373d13f108bfa6210c21
volo <89660634+wolo1@users.noreply.github.com>
2024-01-28 13:16:11 +0100

    Handanimation added on shoot

--- scenes/UIinGame.gd
+++ scenes/UIinGame.gd
@@ -4,12 +4,15 @@ extends Control
 var current_bread_count: int = 100
 @export var max_bread_count: int = 100
 
+signal bread_thrown
+
 func _ready():
 	current_bread_count = max_bread_count
 
 
 func _on_bread_thrown():
 	current_bread_count -= 0.1
+	bread_thrown.emit()
 	update_ui()
 	
 
@@ -18,6 +21,3 @@ func update_ui():
 		current_bread_count = 0
 	gameText.text = str(current_bread_count) + "/" + str(max_bread_count)
 
-
-
-	
